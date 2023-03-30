80512294f85a17c42726b8b37fd81afebf72b3a3
AkhileshUG <83776577+AkhileshUG@users.noreply.github.com>
2023-03-30 17:26:07 +0530

    reverse_LL

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+node *reverse(node *&head)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    while (currptr != NULL)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+
+    return prevptr;
+}
+
+node *reverseRecursive(node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    node *newHead = reverseRecursive(head->next);
+    head->next->next = head;
+    head->next = NULL;
+
+    return newHead;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+
+    display(head);
+
+    // node* rev = reverse(head);
+
+    node *rev = reverseRecursive(head);
+
+    display(rev);
+
+    return 0;
+}
